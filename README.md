2f54ec717f05c402f49bd3d2b2889d7d6d76bf5d
MAACHOU Khalil <khalilmaachou.99@gmail.com>
2023-01-16 13:38:03 +0100

    final version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # VoyageurDeCommerce
 Implementation of local search for the Traveling Salesman.
+
+# Execution :
+
+## General command 
+    python3 voyageurDeCommerce.py Instance Name SIZE MAX_DEPL MAX_ESSAIE ALGO
+## Value for hill climbing
+    To execute this problem using Hill climbing algorithm you need to use these values :
+    Instance Name = filename
+    SIZE = 0
+    MAX_DEPL = number of steps you want
+    MAX_ESSAIE = numbre of tries yu want
+
+## Value for hill climbing
+    To execute this problem using Tabou algorithm you need to use these values :
+    Instance Name = filename
+    SIZE = SIZE of tabou list you want
+    MAX_DEPL = number of steps you want
+    MAX_ESSAIE = 0
--- voyageurDeCommerce.py
+++ voyageurDeCommerce.py
@@ -3,6 +3,7 @@ import sys
 import random
 import math
 import copy
+import time
 
 def lire_instance(filename):
     with open(filename, 'r') as f:
@@ -17,8 +18,7 @@ def lire_instance(filename):
 
 #2.1
 def init_solution(n):
-    solution_init = random.sample(range(1,n+1), n)
-    return solution_init
+    return np.random.permutation(n)
 
 #2.2
 def euclidean_distance(point1, point2):
@@ -28,15 +28,9 @@ def euclidean_distance(point1, point2):
         distance += (point1[i] - point2[i]) ** 2
     return math.sqrt(distance)
 
-def calculer_solution(XY, s, n):
-    valeur = euclidean_distance([0,0], XY[s[0]-1])
-    
-    for i in range(n-1):
-        valeur+= euclidean_distance(XY[s[i]-1], XY[s[i+1]-1])
-    
-    valeur += euclidean_distance(XY[s[n-1]-1], [0,0])
-    
-    return valeur
+def calculer_solution(cities, X, n):
+        return np.linalg.norm(cities[X[0]]) + np.linalg.norm(cities[X[-1]]) + np.linalg.norm(cities[X[1:]] - cities[X[:-1]], axis=1).sum()
+
 
 #2.3
 def meilleur_voisin(XY, s, n):
@@ -46,12 +40,14 @@ def meilleur_voisin(XY, s, n):
     for i in range(n):
         for j in range(i+1,n):
         
-            s_loc = copy.copy(s)
+            s_loc = np.copy(s)
             s_loc[i], s_loc[j] = s_loc[j], s_loc[i]
             val_loc = calculer_solution(XY, s_loc, n)
         
-            if val_loc <= val_min:
+            if val_loc == val_min:
                 solutions.append(s_loc)
+            elif val_loc < val_min:
+                solutions = [s_loc]
                 val_min = val_loc
     
     indice = random.randint(0, len(solutions) - 1)
@@ -61,7 +57,9 @@ def meilleur_voisin(XY, s, n):
 def meilleur(s_prime, s, XY, n):
     return calculer_solution(XY, s, n) > calculer_solution(XY ,s_prime ,n)
 
-def hill_climbing_red(XY ,max_essais ,n):
+def hill_climbing_red(XY ,max_essais ,n, max_depl):
+    start = time.process_time()
+    result = math.inf, 0
     # On effectue plusieurs essais de hill climbing
     for i in range(max_essais):
         # solution initiale au hasard
@@ -71,45 +69,59 @@ def hill_climbing_red(XY ,max_essais ,n):
         nb_depl = 0
 
         STOP = False
-        while not STOP:
+        while not STOP and nb_depl < max_depl:
             # On cherche le meilleur voisin
             voisin = meilleur_voisin(XY ,solution, n)
 
             if meilleur(voisin, solution, XY, n):
                 solution = voisin
+                nb_depl += 1
             else:
                 STOP = True
                 
-            nb_depl += 1
-
+        valeur = calculer_solution(XY,solution,n)
+        if result[0] > valeur:
+            result = valeur, nb_depl 
         print(f"Solution finale = {solution} valeur => {calculer_solution(XY,solution,n)} (nombre de déplacements = {nb_depl})")
+    end = time.process_time()
+    print(end-start)
 
 #2.5
+def in_table(s, Tabou):
+    for s_t in Tabou:
+        if np.all(s==s_t):
+            return True
+    return False
+
 def meilleur_voisin_non_tabou(XY ,Tabou ,s ,n):
     val_min = math.inf
     solutions = []
+    
 
     for i in range(n):
         for j in range(i+1,n):
         
-            s_loc = copy.copy(s)
+            s_loc = np.copy(s)
             s_loc[i], s_loc[j] = s_loc[j], s_loc[i]
             val_loc = calculer_solution(XY, s_loc, n)
         
-            if (val_loc <= val_min) and (s_loc not in Tabou):
+            if (val_loc == val_min) and (not in_table(s_loc, Tabou)):
                 solutions.append(s_loc)
+            elif (val_loc < val_min) and (not in_table(s_loc, Tabou)):
+                solutions = [s_loc]
                 val_min = val_loc
     
     if len(solutions) != 0:
         indice = random.randint(0, len(solutions) - 1)
         return solutions[indice] 
     else:
-        return None
+        return np.array([])
 
 def tabou(XY, max_depl, n, tabou_size):
     # solution initiale au hasard
+    start = time.process_time()
     solution = init_solution(n)
-    print(f"MAX_DEPL {max_depl} : solution initiale = {solution}")
+    print(f"MAX_DEPL {max_depl} : tabou size = {tabou_size}")
 
     # initialise la liste tabou
     Tabou = []
@@ -120,25 +132,38 @@ def tabou(XY, max_depl, n, tabou_size):
 
     while (nb_depl < max_depl) and (not STOP):
         voisin = meilleur_voisin_non_tabou(XY, Tabou, solution, n)
-        if voisin == None:
+        if len(voisin) == 0:
             STOP = True
-        
-        Tabou.append(solution)
-        if meilleur(voisin, msol, XY, n):
-            msol = voisin
-        solution = voisin
-        nb_depl += 1
-        if len(Tabou) > tabou_size:
-            Tabou.pop(0)
+        else:
+            Tabou.append(solution)
+            if meilleur(voisin, msol, XY, n):
+                msol = voisin
+            solution = voisin
+            nb_depl += 1
+            if len(Tabou) > tabou_size:
+                Tabou.pop(0)
 
-    print(f"Solution finale = {solution} valeur => {calculer_solution(XY,msol,n)} (nombre de déplacements = {nb_depl})")
+    end = time.process_time()
+    print(f"Valeur du Solution finale = {calculer_solution(XY,msol,n)} en {end-start} scs (nombre de déplacements = {nb_depl})")
 
 
-if len(sys.argv) != 3:
-    print("Needed Instance Name, MAX_ESSAIE")
+if len(sys.argv) != 6:
+    print("Needed Instance Name, SIZE, MAX_DEPL, MAX_ESSAIE, ALGO")
     sys.exit(1)
 
 XY, n = lire_instance(sys.argv[1])
-MAX_ESSAIE = int(sys.argv[2])
-tabou(XY,10, n, 10)
+SIZE, MAX_DEPL, MAX_ESSAIE, ALGO = int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])
+
+
+if ALGO == 1:
+    hill_climbing_red(XY, MAX_ESSAIE, n, MAX_DEPL)
+else:
+    tabou(XY, MAX_DEPL, n, SIZE)
+
+
+
+
+
+
+
 
